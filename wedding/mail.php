10bd287847d1f665da31d15b6d5cf65e9c981637
maxboens <maxboens@gmail.com>
2022-09-04 15:37:32 +0200

    update

--- wedding/mail.php
+++ wedding/mail.php
@@ -1,16 +1,40 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $guests = $_POST['guests'];
-    $message = $_POST['message'];
-
-    $email_from = $email
-    $email_subject = "New Form submission";
-    $email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
-
-    $to = "weddingmaxlaura@gmail.com";
-    $headers = "From: $email_from \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
+<?php
+    echo "Php works";
+    $name = $_POST['name'];
+
+    $email = $_POST['email'];
+
+    $guests = $_POST['guests'];
+
+    $message = $_POST['message'];
+
+
+
+    $email_from = $email;
+
+    $email_subject = "New Form submission";
+
+    $email_body = "You have received a new message from the user $name.\n".
+
+                            "Here is the message:\n $message".
+
+
+
+    $to = "weddingmaxlaura@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+
+
+    $sendmail = mail($to,$email_subject,$email_body);
+    if($sendMail)
+    {
+    echo "Email Sent Successfully";
+    }
+    else
+
+    {
+    echo "Mail Failed";
+    }
 ?>
+
